f6fde00393ee656686bd265709821c659e5ad6a3
Sushant Sharma <sushant.wurk@gmail.com>
2024-01-06 18:15:50 +0530

    initial commit

---
+++ lua/vesper/colors.lua
@@ -0,0 +1,36 @@
+local M = {}
+
+---@class Palette
+M.default = {
+  none = "NONE",
+
+  bg = "#101010",
+  bg_light = "#1C1C1C",
+  bg_dark = "#161616",
+
+  fg = "#FFF",
+
+  gray = "#7E7E7E",
+  gray = "#707070",
+  gray_light = "#A0A0A0",
+  gray_dark = "#505050",
+  gray = "#282828",
+  gray = "#666",
+  gray = "#343434",
+
+  black = "#000",
+  black_light = "#232323",
+
+  orange = "#FF7300",
+  gold = "#FFC799",
+  gold_light = "#FFCFA8",
+  pepermint = "#99FFE4",
+  red = "#FF8080",
+
+  git = { change = "#6183bb", add = "#449dab", delete = "#914c54" },
+  gitSigns = {
+    add = "#266d6a",
+    change = "#536c9e",
+    delete = "#b2555b",
+  },
+}
